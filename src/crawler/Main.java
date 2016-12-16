ba19173343777e985a5347edc2acec291c51ea4d
Karl-Hendrik Muuga <karl.muuga@gmail.com>
2016-12-16 11:44:00 +0200

    Added input functionality to Main class

--- src/crawler/Main.java
+++ src/crawler/Main.java
@@ -1,10 +1,31 @@
 package crawler;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import lib.TextIO;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException, IOException {
 
+		// Ask for input
+		System.out.println("Hello! \nWhat are you looking for?");
+		String searchFor = TextIO.getlnString();
+
+		System.out.println("And where do you want to start?");
+		String URI = TextIO.getlnString();
+
+		if (URI.contains("http") || URI.contains("https")) {
+			// Everything seems to be fine :)
+		} else {
+			// Add the necessary prefix
+			URI = "http://" + URI;
+		}
+
+		// Empty the database table of old URL-s before crawling again
+		Crawler.db.boolQuery("TRUNCATE links;");
+
+		// Start
+		Crawler.startingPoint(URI, searchFor);
 	}
-
 }
